a1b386b7c3cd84aaca0ef3f1cf52771c7c36325d
Chris Hewison <hewisonchris@gmail.com>
2021-06-04 14:52:28 +0900

    Extracted Config to a module
    The Faucet `main.d` file is getting a bit too long for convenience so
    splitting out the configuration code helps. Perhaps some more extraction
    can follow later.

--- source/faucet/main.d
+++ source/faucet/main.d
@@ -16,6 +16,7 @@
 module faucet.main;
 
 import faucet.API;
+import faucet.config;
 import faucet.stats;
 
 import agora.api.FullNode;
@@ -30,9 +31,6 @@ import agora.stats.Server;
 import agora.stats.Utils;
 import agora.utils.Test;
 import agora.script.Lock;
-
-import dyaml.node;
-import dyaml.loader;
 
 import std.algorithm;
 import std.exception;
@@ -61,86 +59,6 @@ private SecretKey[PublicKey] secret_keys;
 
 /// The configuration for faucet as a faucet and a tx generator
 private Config config;
-
-private struct TxGenerator
-{
-    /// How frequently we run our periodic task
-    ulong send_interval;
-
-    /// Between how many addresses we split a transaction by
-    uint split_count;
-
-    /// Maximum number of utxo before merging instead of splitting
-    uint merge_threshold;
-
-    /// Addresses to send the transactions to
-    string[] addresses;
-
-    /// Keys from the config
-    string[] keys;
-
-    /// Stats port (default: 9113)
-    ushort stats_port;
-
-    this (ulong send_interval, uint split_count, uint merge_threshold,
-        string[] addresses, string[] keys, ushort stats_port = 9113)
-    {
-        this.send_interval = send_interval;
-        this.split_count = split_count;
-        this.merge_threshold = merge_threshold;
-        this.addresses = addresses;
-        this.keys = keys;
-        this.stats_port = stats_port;
-    }
-
-    this (Node yaml_node) @safe
-    {
-        send_interval = yaml_node["send_interval"].as!ulong;
-        split_count = yaml_node["split_count"].as!uint;
-        merge_threshold = yaml_node["merge_threshold"].as!uint;
-        () @trusted { addresses = parseSequence("addresses", yaml_node, true); }();
-        () @trusted { keys = parseSequence("keys", yaml_node, false); }();
-        stats_port = yaml_node["stats_port"].as!ushort;
-    }
-}
-
-/// The Config for the faucet web
-private struct Web
-{
-    /// Address to bind for website
-    string address;
-
-    /// Port to bind for website
-    ushort port;
-
-    this (string address, ushort port = 2766)
-    {
-        this.address = address;
-        this.port = port;
-    }
-
-    this (Node yaml_node) @safe
-    {
-        this.address = yaml_node["address"].as!string;
-        this.port = yaml_node["port"].as!ushort;
-    }
-}
-
-/// Configuration parameter for Faucet
-private struct Config
-{
-    /// configuration for tx generator
-    TxGenerator tx_generator;
-
-    /// config for faucet web
-    Web web;
-
-    this (TxGenerator tx_generator, Web web)
-    {
-        this.tx_generator = tx_generator;
-        this.web = web;
-    }
-}
 
 /// Override the symbol 'TxBuilder' and use the Config as a default
 public struct Builder
@@ -344,6 +262,7 @@ public class Faucet : FaucetAPI
           A client to send transactions or requests
 
     *******************************************************************************/
+
     private API randomClient () @trusted
     {
         return choice(this.clients);
@@ -433,7 +352,7 @@ public class Faucet : FaucetAPI
             assert(utxo_len >= 8);
             this.splitTx(this.state.utxos.storage.byKeyValue(), 100)
                 .take(8)
-                .each!(tx => randomClient().putTransaction(tx));
+                .each!(tx => this.randomClient().putTransaction(tx));
             this.faucet_stats.increaseMetricBy!"faucet_transactions_sent_total"(8);
         }
     }
@@ -482,7 +401,7 @@ public class Faucet : FaucetAPI
         if (this.state.utxos.storage.length > config.tx_generator.merge_threshold)
         {
             auto tx = this.mergeTx(this.state.utxos.byKeyValue().take(uniform(10, 100, rndGen)));
-            randomClient().putTransaction(tx);
+            this.randomClient().putTransaction(tx);
             logDebug("Transaction sent: %s", tx);
             this.faucet_stats.increaseMetricBy!"faucet_transactions_sent_total"(1);
         }
@@ -492,7 +411,7 @@ public class Faucet : FaucetAPI
                 .take(uniform(1, 10, rndGen));
             foreach (tx; rng)
             {
-                randomClient().putTransaction(tx);
+                this.randomClient().putTransaction(tx);
                 logDebug("Transaction sent: %s", tx);
                 this.faucet_stats.increaseMetricBy!"faucet_transactions_sent_total"(1);
             }
@@ -525,7 +444,7 @@ public class Faucet : FaucetAPI
         {
             Transaction tx = txb.draw(leftover, [pubkey]).sign();
             logInfo("Sending %s BOA to %s", amount.coins, recv);
-            randomClient().putTransaction(tx);
+            this.randomClient().putTransaction(tx);
             this.faucet_stats.increaseMetricBy!"faucet_transactions_sent_total"(1);
         }
         else
@@ -554,7 +473,7 @@ public class Faucet : FaucetAPI
 
             Transaction tx = txb.sign();
             logInfo("Sending %s BOA to %s", amount.coins, recv);
-            randomClient().putTransaction(tx);
+            this.randomClient().putTransaction(tx);
             this.faucet_stats.increaseMetricBy!"faucet_transactions_sent_total"(1);
         }
     }
@@ -695,36 +614,6 @@ private string getStaticFilePath ()
                         "This might mean your faucet is not installed correctly. " ~
                         "Searched for `index.html` in '" ~ std.file.getcwd() ~
                         "/frontend/'.");
-}
-
-/// Parse the config section
-private string[] parseSequence (string section,
-        Node root, bool optional = false)
-{
-    if (auto yaml_node = section in root)
-        enforce(root[section].type == NodeType.sequence,
-            format("`%s` section must be a sequence", section));
-    else if (optional)
-        return null;
-    else
-        throw new Exception(
-            format("The '%s' section is mandatory and must " ~
-                "specify at least one item", section));
-
-    string[] result;
-    foreach (string item; root[section])
-        result ~= item;
-
-    return result;
-}
-
-/// Parse the config file
-public Config parseConfigFile (string configPath)
-{
-    Node root = Loader.fromFile(configPath).load();
-    return Config(
-        TxGenerator(root["tx_generator"]),
-        Web(root["web"]));
 }
 
 /// Global because we need to access it from our signal handler
